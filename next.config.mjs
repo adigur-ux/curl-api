08794bfffaa7775791704dc8cdba6e0207b8f9e9
adigur-ux <adigur@gmail.com>
2025-10-06 11:08:53 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   async rewrites() {
-    if (process.env.NODE_ENV === "development") {
-      return [
-        {
-          source: "/api/zap/:path*",
-          destination: "https://curl-api-zeta.vercel.app/:path*",
-        },
-      ];
-    }
-    return [];
+    return [
+      {
+        source: "/api/zap/:path*",
+        destination: "https://curl-api-zeta.vercel.app/:path*",
+      },
+    ];
   },
 };
+
 export default nextConfig;
